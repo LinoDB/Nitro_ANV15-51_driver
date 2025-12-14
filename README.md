72e01dbf9de3e0fa8d5ada049c173d5b6a34a49b
LinoDB <linodelbianco@web.de>
2025-12-14 16:05:01 -0600

    Add wmi driver initialization and functionality to read the battery charge mode.

--- README.md
+++ README.md
@@ -2,6 +2,9 @@ Creating a driver for the Nitro ANV15-51 laptop to make use of WMI functions.
 
 This is a project to learn about driver development on linux.
 
-### Sources (until now)
+### Resources
 - https://lwn.net/Kernel/LDD3/
 - https://github.com/torvalds/linux/blob/master/Documentation/wmi/driver-development-guide.rst
+- https://docs.kernel.org/driver-api/wmi.html#c.wmi_driver
+- https://github.com/0x7375646F/Linuwu-Sense
+- https://github.com/DetuxTR/AcerNitroLinuxGamingDriver
--- src/nitro_battery_control.c
+++ src/nitro_battery_control.c
@@ -10,6 +10,10 @@
 #include <linux/slab.h>
 #include <asm/uaccess.h>
 #include <linux/errno.h>
+#include <linux/acpi.h>
+#include <linux/wmi.h>
+
+#define BATTERY_CONTROL_GUID "79772EC5-04B1-4bfd-843C-61E7F77B6CC9"
 
 
 /************************************
@@ -56,15 +60,111 @@ struct nitro_battery {
     int major;
     int minor;
     bool initialized;
-    bool active;
     const char* name;
 };
 
 static struct nitro_battery _device = {
     .initialized = false,
-    .active = false,
     .name = "nitro_battery"
 };
+
+static const struct wmi_device_id batt_dev_id = {
+    .guid_string = BATTERY_CONTROL_GUID
+};
+
+static struct wmi_device* batt_wdev;
+
+static int batt_probe(struct wmi_device *wdev, const void *context) {
+    if(!wmi_has_guid(BATTERY_CONTROL_GUID)) {
+        printk(KERN_ERR "Nitro Battery Control Driver init error: WMI device GUID couldn't be found");
+        return -ENODEV;
+    }
+    batt_wdev = wdev;
+    return 0;
+}
+
+static void batt_remove(struct wmi_device *wdev) {
+    if(batt_wdev) batt_wdev = NULL;
+}
+
+static struct wmi_driver batt_driver = {
+    .driver = {
+        .owner = THIS_MODULE,
+        .name = "Battery health WMI driver",
+        .probe_type = PROBE_PREFER_ASYNCHRONOUS
+    },
+    .id_table = &batt_dev_id,
+    .probe = &batt_probe,
+    .remove = &batt_remove,     // needed?
+    .shutdown = &batt_remove    // needed?
+};
+
+struct __attribute__((packed)) battery_charge_limit_in {
+    u8 uBatteryNo;
+    u8 uFunctionQuery;
+    u8 uReserved[2];
+};
+
+static struct battery_charge_limit_in check_charge_limit_in = {
+    .uBatteryNo = 1,
+    .uFunctionQuery = 1,
+    .uReserved = {0, 0}
+};
+
+struct __attribute__((packed)) battery_charge_limit_out {
+    u8 uFunctionList;
+    u8 uReturn[2];
+    u8 uFunctionStatus[5];
+};
+
+struct wmi_method_input {
+    const struct acpi_buffer in;
+    u8 instance;
+    u32 method_id;
+};
+
+static struct wmi_method_input read_battery_charge_limited = {
+    .in = { sizeof(struct battery_charge_limit_in), &check_charge_limit_in },
+    .instance = 0,
+    .method_id = 20
+};
+
+
+/************************************
+************ WMI methods ************
+************************************/
+
+// turn this into a generic call that returns a defined output class
+static int check_battery_charge_limited(void) {
+    // thanks to 0x7375646F: https://github.com/0x7375646F/Linuwu-Sense/blob/df84ac7a020efebd4cd1097e73940d93eb959093/src/linuwu_sense.c#L3001
+    if(!batt_wdev) return -1;
+    struct acpi_buffer out = { ACPI_ALLOCATE_BUFFER, NULL };
+    acpi_status status = wmidev_evaluate_method(
+        batt_wdev,
+        read_battery_charge_limited.instance,
+        read_battery_charge_limited.method_id,
+        &read_battery_charge_limited.in,
+        &out
+    );
+    if(ACPI_FAILURE(status)) {
+        printk(KERN_ERR "Nitro Battery Control Driver: Battery status couldn't be read\n");
+        return -1;
+    }
+    union acpi_object* obj = out.pointer;
+    int enabled = -1;
+    if (!obj || obj->type != ACPI_TYPE_BUFFER || obj->buffer.length != sizeof(struct battery_charge_limit_out)) {
+        printk(
+            KERN_ERR "Nitro Battery Control Driver: Battery status returned wrong buffer: type %d, length %d\n",
+            obj->type,
+            obj->buffer.length
+        );
+    }
+    else {
+        enabled = ((struct battery_charge_limit_out *)obj->buffer.pointer)->uFunctionStatus[0];
+    }
+    kfree(obj);
+    return enabled;
+}
 
 
 /************************************
@@ -84,16 +184,21 @@ static ssize_t nitro_battery_read(
     loff_t* ppos
 ) {
     if(*ppos > 0) return 0;
-    struct nitro_battery* dev = file->private_data;
+    int status = check_battery_charge_limited();
     char* ret;
     size_t actual_count = 0;
-    if(dev->active) {
-        ret = "activated\n";
-        actual_count = 10 * sizeof(char);
-    }
-    else {
-        ret = "deactivated\n";
-        actual_count = 12 * sizeof(char);
+    switch(status) {
+        case 1:
+            ret = "activated\n";
+            actual_count = 10 * sizeof(char);
+            break;
+        case 0:
+            ret = "deactivated\n";
+            actual_count = 12 * sizeof(char);
+            break;
+        default:
+            ret = "unknown\n";
+            actual_count = 8 * sizeof(char);
     }
     if(copy_to_user(buf, ret, actual_count)) {
         return -EFAULT;
@@ -108,17 +213,17 @@ static ssize_t nitro_battery_write(
     size_t count,
     loff_t* ppos
 ) {
-    struct nitro_battery* dev = file->private_data;
+    // struct nitro_battery* dev = file->private_data;
     char activate[1];
     if(copy_from_user(activate, buf, 1)) {
         return -EFAULT;
     }
     char compstr[] = "01";
     if(*activate == compstr[0]) {
-        dev->active = false;
+        // dev->active = false;
     }
     else if(*activate == compstr[1]) {
-        dev->active = true;
+        // dev->active = true;
     }
     else {
         printk(KERN_WARNING "Undefined input for Nitro Battery Control Driver: %s\n", activate);
@@ -170,7 +275,7 @@ static int __init nitro_battery_init(void) {
     // create c_dev device
     cdev_init(&_device.cdev, &fops);
     _device.cdev.owner = THIS_MODULE;
-    _device.cdev.ops = &fops; // necessary?
+    _device.cdev.ops = &fops; // needed?
 
     // register c_dev device
     err = cdev_add(&_device.cdev, _device.devno, 1);
@@ -179,6 +284,9 @@ static int __init nitro_battery_init(void) {
         goto unreg_dev;
     }
 
+    if(wmi_driver_register(&batt_driver)) {
+        printk(KERN_ERR "Nitro Battery Control Driver init error: Couldn't register battery device driver");
+    }
     printk(KERN_INFO "Initialized Nitro Battery Control driver with major number: %d!\n", _device.major);
     return 0;
 
@@ -196,6 +304,9 @@ static void __exit nitro_battery_exit(void) {
     }
     else {
         printk(KERN_INFO "Nitro Battery Control driver wasn't initialized properly!\n");
+    }
+    if(batt_wdev) {
+        wmi_driver_unregister(&batt_driver);
     }
 }
 
